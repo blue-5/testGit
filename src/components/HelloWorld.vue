420b953b1613892d8a7493e8f2426535bef9c355
蓝静怡 <lanjingyi@countrygarden.com.cn>
2020-02-09 21:06:03 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -90,13 +90,20 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  mounted () {
+    this.test()
+  },
+  test () {
+    console.log('object')
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
